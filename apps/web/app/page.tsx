3de53fcfcefc29b7d8340ddae44b5f086c6f16f3
Roman <spielerua@gmail.com>
2023-07-10 18:39:24 +0300

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ import api from "../services/api";
 
 const getData = async () => {
   const res = await api("/homepage", {
-    cache: "no-cache"
+    cache: "no-store"
   });
   return res.json();
 };
